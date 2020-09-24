e7d0b5a4d93a2c4b5b1567975565938921dfca5b
Roger <rrosset91@gmail.com>
2020-09-24 18:53:08 -0300

    pos-animacoes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 console.log("Flappy Bird - Roger Rosset. Feito com o Guia de Dev Soutinho");
-
+let frames = 0;
 const sprites = new Image();
 sprites.src = "./img/sprites.png";
 const canvas = document.querySelector("canvas");
@@ -40,17 +40,34 @@ const bird = {
       changeScreen(screens.START);
       return;
     }
-    bird.upper = bird.canvasY;
-    bird.bottom = bird.canvasY - bird.height;
     bird.speed = bird.speed + bird.gravity;
     bird.canvasY = bird.canvasY + bird.speed;
   },
+  movementAnimation: [
+    { spriteX: 0, spriteY: 0 },
+    { spriteX: 0, spriteY: 26 },
+    { spriteX: 0, spriteY: 52 },
+  ],
+  actualFrame: 0,
+  updateActualFrame() {
+    //ENTENDER MELHOR
+    const framesInterval = 10;
+    const intervalGone = frames % framesInterval === 0;
 
+    if (intervalGone) {
+      const incrementBase = 1;
+      const increment = incrementBase + bird.actualFrame;
+      const baseRepetition = bird.movementAnimation.length;
+      bird.actualFrame = increment % baseRepetition;
+    }
+  },
   draw() {
+    bird.updateActualFrame();
+    const { spriteX, spriteY } = bird.movementAnimation[bird.actualFrame];
     context.drawImage(
       sprites,
-      bird.spriteX,
-      bird.spriteY,
+      spriteX,
+      spriteY,
       bird.width,
       bird.height,
       bird.canvasX,
@@ -69,7 +86,12 @@ const floor = {
   height: 112,
   canvasX: 0,
   canvasY: canvas.height - 112,
-
+  update() {
+    //ENTENDER MELHOR
+    const repeatOn = floor.width / 2;
+    const movement = floor.canvasX - 1;
+    floor.canvasX = movement % repeatOn;
+  },
   draw() {
     context.drawImage(
       sprites,
@@ -168,6 +190,7 @@ const screens = {};
 screens.START = {
   draw() {
     background.draw();
+    floor.update();
     floor.draw();
     bird.draw();
     startScreen.draw();
@@ -186,6 +209,7 @@ screens.GAME = {
     bird.update();
     background.draw();
     floor.draw();
+    floor.update();
     bird.draw();
   },
   click() {
@@ -222,6 +246,7 @@ function makeCollision(birdY, floorY) {
 function gameLoop() {
   activeScreen.draw();
   activeScreen.update();
+  frames++;
   requestAnimationFrame(gameLoop);
 }
 window.addEventListener("click", function () {
